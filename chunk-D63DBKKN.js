ea8c5264ddd7bce0357772a043550193b60628d6
Jimmy Noborikawa <programador5@globaltecperu.com>
2025-02-17 14:12:39 -0500

    Auto-generated commit

--- chunk-D63DBKKN.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var t=[{path:"listado",loadComponent:()=>import("./chunk-OIMWQIVZ.js").then(o=>o.ListadoPageComponent)},{path:"generar",loadComponent:()=>import("./chunk-CQMPULXC.js").then(o=>o.GenerarPageComponent)},{path:"**",redirectTo:"listado"}];export{t as routes};
